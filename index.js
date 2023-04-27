fec71962b822ea61adf025c713810fcde1f106fc
Salah <dannaouisalah@gmail.com>
2023-04-27 22:16:01 +1000

    first commit, created prompt

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+const generateSVG = require('./lib/svg-generator.js');
+
+const questions = [
+    {
+        type: 'input',
+        message: "Choose your text! (up to 3 letters)",
+        name: 'text',
+    },
+    {
+        type: 'input',
+        message: "Choose your text color by typing in a valid color keyword or a hex number (#).",
+        name: 'textColor',
+    },
+    {
+        type: 'list',
+        message: "Pick a shape.",
+        name: 'shape',
+        choices: ['Triangle', 'Square', 'Circle'],
+    },
+    {
+        type: 'input',
+        message: "Choose your shape color by typing in a valid color keyword or a hex number (#).",
+        name: 'shapeColor',
+    },
+];
+
+inquirer.prompt(questions)
+    .then((result) => generateSVG. (result))
+    .catch((err) => console.log(err))
